f58bafecf1bc4a34ff5311a9e5f2e3e2502d63d2
piet@piet-Latitude-E7440 <pietglas77@gmail.com>
2020-06-19 13:39:34 +0200

    added documentation to the some function declarations in the model

--- ssmodel.h
+++ ssmodel.h
@@ -1,7 +1,7 @@
 /* Author: Piet Glas
  *
  * TODO:
- * - when a formula is removed, update dependencies.
+ * - 
  */
 
 #pragma once
@@ -45,7 +45,10 @@ public:
 	bool getDataFromFile(const QString& file_name);
 	// Save data in a .txt file. 
 	bool saveData(const QString & file_name) const;
-	// Add a formula.
+	// Adds a formula to the cell with index key, and calls 
+	// calculateFormula to set the value of the cell. Returns
+	// true if succesfull, i.e. if no circular dependencies
+	// were caused
 	bool setFormula(const QString & formula, const QString& key);
 
 	// converts a qmodelindex into the 'displayed' index, and vice versa
@@ -58,28 +61,35 @@ public slots:
 	void getFormula(const QModelIndex & current);
 signals:
 	// sends a signal with the formula to sswindow,
-	// so it can be displayed in the statusbar
+	// so the correct formula is displayed in the formula editor
 	void sendFormula(const QString & formula);
 
 private:
 	int rows_;
 	int cols_;
+	// contains the data as <index, <data, formula>>
 	QMap<QString, QPair<QVariant,QVector<QString>>> data_;
+	// keeps track of the cells that each cell depends on
 	QMap<QString, std::set<QString>> depends_on_;
+	// keeps track of the cells that each cell has influnce on
 	QMap<QString, std::set<QString>> has_effect_on_;
 
-	// calculate a formula.
+	// calculate a custom formula, returns the value
 	double calculateFormula(std::shared_ptr<Expression> formula);
-	// checks if a formula causes circular dependencies to occur
+	// calculate a predifined formula, returns the value
 	double calculatePredefinedFormula(const QVector<QString>& tokens);
+	// returns true if rhs depends on lhs, which would cause 
+	// a circular dependency
 	bool checkCircularity(const QString & lhs, 
 					const std::set<QString> & indices_rhs);
 	bool checkCircularityHelper(const QString & lhs,
 			const std::set<QString> & depends_on);
-	// updates dependent values
+	// updates the cells that depend on index
 	void updateDependentValues(const QString & index);
 
-	// helper function for dealing with predefined formulas
+	// returns all indices between index1 and index2, in the following way:
+	// - getIndices(A1:A6) returns {A1, A2, A3, A4, A5, A6}
+	// - getIndices(A1:C2) returns {A1, A2, B1, B2, C1, C2}
 	std::set<QString> getIndices(const QString& index1, const QString& index2) const;
 
 	// functions to calculate predefined formulas
